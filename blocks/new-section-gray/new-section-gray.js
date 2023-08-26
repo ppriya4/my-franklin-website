381d1eac48779ba6806f259b456b7daff9af2e6d
pritampriya <pritamp@adobe.com>
2023-08-27 00:03:21 +0530

    new file:   blocks/new-section-gray/new-section-gray.js
    modified:   blocks/franklin-banner/franklin-banner.css

---
+++ blocks/new-section-gray/new-section-gray.js
@@ -0,0 +1,4 @@
+import { fetchPlaceholders, getMetadata } from '../../scripts/lib-franklin.js';
+export default async function decorate(block) {
+    const placeholders = await fetchPlaceholders(''); 
+}
